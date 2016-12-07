8cbea52c65592a0c9e99770e2beccd3e40e37692
Jinchun Zhang <jz2516@nyu.edu>
2016-12-07 10:12:14 -0500

    Add files via upload

---
+++ MLAssignment-JinchunZhang.sml
@@ -0,0 +1,91 @@
+(*ML Assignment *)
+(*JINCHUN ZHANG*)
+
+Control.Print.printDepth := 100;
+Control.Print.printLength := 100;
+
+(*Q1*)
+fun merge L1 []=L1
+|	merge [] L2=L2
+|	merge (x1::xs) (y1::ys)=
+		if x1<y1 then x1::(merge xs (y1::ys))
+		else y1::(merge ys (x1::xs))
+
+
+(*Q2*)
+fun split []=([], [])
+|	split [x]=([x], [])
+|	split (od::ev::xs) = 
+		let val (ods, evs)=split xs
+		in ((od::ods),(ev::evs))
+	end
+
+(*Q3*)
+fun mergeSort []=[]
+|	mergeSort [x]=[x]
+|	mergeSort L=
+		let val (fh, sf) = split L
+			in merge (mergeSort fh) (mergeSort sf)
+	end
+
+
+(*Q4*)
+infix <;
+fun sort (op <) []=[]
+|	sort (op <) [x]=[x]
+|	sort (op <) L=let
+		fun merge (L1, [])=L1
+		|	merge ([], L2)=L2
+		|	merge ((x1::xs), (y1::ys))= 
+				if x1 < y1 then x1::(merge (xs, (y1::ys)))
+				else y1::(merge (ys, (x1::xs)))
+		fun split [] = ([],[])
+		|	split [x]=([x],[])
+		|	split (od::ev::ms)=let
+				val(ods, evs)=split ms
+				in (od::ods, ev::evs)
+			end
+		val (fh, sf)=split L
+		in merge (sort (op <) fh,  sort (op <) sf)
+	end
+
+(*Q5*)
+datatype 'a tree = empty | leaf of 'a | node of 'a *'a tree * 'a tree
+
+(*Q6*)
+fun labels (empty: 'a tree) =[]
+|	labels (leaf x) =[x]
+|	labels (node(y, t1, t2))= labels t1 @ [y] @ labels t2
+
+(*Q7*)
+infix == ;
+fun replace (op ==) (x:'a) (y:'a) (empty: 'a tree) = empty
+|	replace (op ==) (x: 'a) (y: 'a) (leaf m) = 
+			if x == m then leaf(y) else leaf(m)
+|	replace (op ==) (x:'a) (y:'a) (node(m,t1,t2)) = 
+			if x == m then node(y, replace (op ==) x y t1, replace (op ==) x y t2)
+			else node(m, replace (op ==) x y t1, replace (op ==) x y t2)
+
+
+(*Q8*)
+fun replaceEmpty (y: 'a tree) (empty: 'a tree) = y
+|	replaceEmpty (y: 'a tree) (leaf m: 'a tree) = leaf(m)
+|	replaceEmpty (y: 'a tree) (node(x, t1, t2): 'a tree) = node(x, replaceEmpty y t1, replaceEmpty y t2)
+
+
+(*Q9*)
+(*define a function increment*)
+fun increment empty = leaf 0
+|	increment (leaf a)=leaf (a+1)
+|	increment (node(a, L,R))=node(a+1, L, R)
+
+fun mapTree f empty = f empty
+ |  mapTree f (leaf a) = f (leaf a)
+ |  mapTree f (node (a,t1,t2)) = f (node (a, mapTree f t1, mapTree f t2))
+
+
+(*Q10*)
+fun sortTree (op <) T = mapTree (fn empty => empty
+                                  | (leaf x) => leaf (sort (op <) x)
+                                  | (node (x,t1,t2)) => node (sort (op <) x,t1,t2)) T
+
---
+++ assignment.scm
@@ -0,0 +1,101 @@
+;;;PL Homework -scheme
+;;; Jinchun Zhang
+;;;Q1
+;;; Base Case: L is empty, return 0
+;;; Assumption: (count-numbers M) returns a count of the numbers in M, for
+;;; any list M smaller than L (including (car L) and (cdr L)).
+;;; Step: If (car L) is a list, then return the sum of the count of the
+;;; numbers in (car L) and the count of the numbers in (cdr L).
+;;; If (car L) is a number, return 1 plus the count of the numbers in
+;;; (cdr L). Otherwise, return the count of the numbers in (cdr L).
+
+(define (count-numbers L)
+	(cond ( (null?  L)  0)
+              (else (cond ((list? (car L)) (+ (count-numbers (car L)) (count-numbers (cdr L))))
+                          (else (cond ((number? (car L)) (+ 1 (count-numbers (cdr L))))
+                                      (else (count-numbers (cdr L)))))))))
+
+(count-numbers '(11 (22 (a 33 44) 55) (6 (b 7 8 (9 c) 100 d))))
+
+;;;Q2
+;;; Base Case: L is empty, return (x)
+;;; Assumption: ( insert x M) returns a sorted list containing x and all the elements in M, for
+;;; any list M smaller than L (including (car L) and (cdr L)).
+;;; Step: If (car L) is bigger than x then then return list that has x as the first element.
+;;; i.e. return (cons x L)
+;;; If (car L) is smaller than x, then return the list that has  (car L) as the first element
+;;; and insert x in the position afterwards. (cons (car L) (insert x (cdr L))). 
+
+(define  (insert x L) 
+	(cond ( (null? L)  (list x) )
+	(else (cond ( (> (car L) x)  (cons x L) ) 
+                     (else( cons (car L)  (insert x ( cdr L))))))))
+ (insert 5 '(1 2 3 4 6 7 8))
+
+;;;Q3
+;;; Base Case: L is empty, return (M)
+;;; Assumption: ( insert-all Q M) returns a sorted list containing a all the elements in M and Q, for
+;;; any list Q smaller than L (including (car L) and (cdr L)).
+;;; Step: call insert ( car L)  (insert-all (cdr L) M)
+
+(define (insert-all L M)
+(cond ( (null? L) M )
+(else (insert (car L) (insert-all (cdr L) M)))))
+
+ (insert-all '(2 4) '(1 3 5))
+
+;;;Q4
+;;; Base Case: L is empty, return (L)
+;;; Assumption: (sort M) returns a sorted list containing all the elements in M, for
+;;; any list M smaller than L (including (car L) and (cdr L)).
+;;; Step: call  insert ( car(L)  (sort (cdr L) ) )
+
+(define sort (lambda (L)
+           (letrec
+              ((insert  (lambda (x L)
+                 (cond((null? L) (list x))
+                    (else (cond ( (<= x (car L)) (cons x L))
+                           (else (cons (car L) (insert x (cdr L))))))))))
+            (cond ( (null? L) L)
+                  ( else (insert (car L) (sort (cdr L))))))))
+
+(sort '(3 6 1 5 2 7 4))
+
+;;;Q5
+;;; Translate one by one
+(define (translate op)
+  (cond ((eq? op '+) +)
+        ((eq? op '*) *)
+        ((eq? op '-) -)
+        ((eq? op '/) /)))
+
+((translate '*) 3 4)
+
+
+;;;Q6
+;;;Base case: the exp is a number then return it;
+;;;Assumption: (postfix-eval L) works for any element L inside the exp. including (car L) and (caar L
+;;;Step: call ((translate (cddr L)) (postfix-eval (car L)) (postfix-eval (caar L)))
+
+(define (postfix-eval exp)
+  (cond ((number? exp) exp)
+        (else ((translate (caddr exp))  (postfix-eval (car exp))  (postfix-eval (cadr exp))))))
+
+(postfix-eval `((16 12 *) ((2 6 +) (9 1 -) *) /))
+
+
+;;; Q7
+;;; Base Case: L is empty, return the set containing the empty
+;;; set, i.e. ’(()).
+;;; Assumption: (powerset M) returns the powerset of M, for any set M smaller than L (including (cdr L)).
+;;; Step: append (powerset (cdr L)) and the list hat having (car L) in the front of each elements
+;;; in (powerset (cdr L)).
+
+(define (powerset L)
+  (cond ((null? L) '(()))
+        (else (let ((S (powerset (cdr L))))
+                    (append  S (map (lambda (x) (cons (car L) x)) S))))))
+
+(powerset '(1 2 3 4))
+
+(powerset '(1 2 3 4))
